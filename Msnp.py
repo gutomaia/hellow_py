b7e32b3e0388f8fdd3c9ade704ab76af55e34bde
Guto Maia <guto@guto.net>
2011-07-26 13:16:50 -0300

    style: switching tabs for spaces #pep8

--- Msnp.py
+++ Msnp.py
@@ -9,46 +9,47 @@
 
 import SocketConnection
 
+
 class Msnp:
+    
+    def __init__(self):
+        self.EL = "\r\n"
+        self._trid = 1
+        self._connection = None
+        self._commandListener = None
+        self._connectionHandle = None
+        self._connectionHandle = SocketConnection.SocketConnection()
+    
+    def setConnectionHandle(self, connectionHandle):
+        self._connectionHandle = connectionHandle
+    
+    def addCommandListener(self, commandListener):
+        self._commandListener = commandListener
 
-	def __init__(self):
-		self.EL = "\r\n"
-		self._trid = 1
-		self._connection = None
-		self._commandListener = None
-		self._connectionHandle = None
-		self._connectionHandle = SocketConnection.SocketConnection()
-	
-	def setConnectionHandle(self, connectionHandle):
-		self._connectionHandle = connectionHandle
-	
-	def addCommandListener(self, commandListener):
-		self._commandListener = commandListener
+    def onCommandReceived(self, command):
+        if self._commandListener != None: self._commandListener.onCommandReceived(command)
+    
+    def onCommandSended(self, command):
+        if self._commandListener != None: self._commandListener.onCommandSended(command)
 
-	def onCommandReceived(self, command):
-		if self._commandListener != None: self._commandListener.onCommandReceived(command)
-	
-	def onCommandSended(self, command):
-		if self._commandListener != None: self._commandListener.onCommandSended(command)
+    #abstract function getHost();
+    #abstract function getPort();
+    #abstract function execute($command);
 
-	#abstract function getHost();
-	#abstract function getPort();
-	#abstract function execute($command);
+    def send(self, command):
+        self._connectionHandle.send(command)
+        self.onCommandSended(command)
+        self._trid += 1
 
-	def send(self, command):
-		self._connectionHandle.send(command)
-		self.onCommandSended(command)
-		self._trid += 1
+    def _connect(self, host, port):
+        self._connectionHandle.connect(host, port)
+        
+    def disconnect(self):
+        self._connectionHandle.disconnect()
 
-	def _connect(self, host, port):
-		self._connectionHandle.connect(host, port)
-		
-	def disconnect(self):
-		self._connectionHandle.disconnect()
-
-	def listen(self):
-		while self._connectionHandle.hasMoreCommands():
-			command = self._connectionHandle.nextCommand()
-			if command != None and command != "":
-				self.execute(command)
-				self.onCommandReceived(command)
+    def listen(self):
+        while self._connectionHandle.hasMoreCommands():
+            command = self._connectionHandle.nextCommand()
+            if command != None and command != "":
+                self.execute(command)
+                self.onCommandReceived(command)
--- Msnp8.py
+++ Msnp8.py
@@ -9,81 +9,80 @@
 
 import Msnp
 import Notification
- 
+
 class Msnp8(Notification.Notification):
+    def __init__(self):
+        Notification.Notification.__init__(self)
+        #super(Msnp8,self).connect()
+        self.MSN_HOST = 'messenger.hotmail.com'
+        self.MSN_PORT = 1863
+        self.PROTOCOL_VERSION = 'MSNP8'
+        self.LOCALE_ID = '0x0409'
 
-	def __init__(self):
-		Notification.Notification.__init__(self)
-		#super(Msnp8,self).connect()
-		self.MSN_HOST = 'messenger.hotmail.com'
-		self.MSN_PORT = 1863
-		self.PROTOCOL_VERSION = 'MSNP8'
-		self.LOCALE_ID = '0x0409'
+        self.OS_TYPE = 'win'
+        self.OS_VERSION = '4.10'
+        self.CPU_ARCHITECTURE = 'i386'
+        self.CLIENT_NAME = 'MSNMSGR'
+        self.CLIENT_VERSION = '6.0.0602'
 
-		self.OS_TYPE = 'win'
-		self.OS_VERSION = '4.10'
-		self.CPU_ARCHITECTURE = 'i386'
-		self.CLIENT_NAME = 'MSNMSGR'
-		self.CLIENT_VERSION = '6.0.0602'
+        #Challenger
 
-		#Challenger
+        self.CLIENT_ID = 'MSMSGS'
+        self.CLIENT_IDCODE = 'msmsgs@msnmsgr.com'
+        self.CLIENT_CODE = 'Q1P7W2E4J9R8U3S5'; #needed for the chalenger
 
-		self.CLIENT_ID = 'MSMSGS'
-		self.CLIENT_IDCODE = 'msmsgs@msnmsgr.com'
-		self.CLIENT_CODE = 'Q1P7W2E4J9R8U3S5'; #needed for the chalenger
-
-	def execute(self, command):
-		cmd = command[:3]
-		if cmd == 'VER':
-			self.send(self.cvr())
-		elif cmd == "CVR":
-			self.send(self.usr())
-		elif cmd == "XFR":
-			host_port = command.split(' ')[3].split(':')
-			host = host_port[0]
-			port = host_port[1]
-			self.connect(host, port)
-		elif cmd == "USR":
-			params = command.split(' ')
-			if params[2] == "TWN":
-				self.authenticate(params[4])
-				self.send(self.usr())
-			elif params[2] == "OK":
-				#self.onLogged()
-				self.send(self.syn())
-		elif cmd == "SYN":
-			self.send(self.chg)
-#		elif cmd == "GTC":
-#		elif cmd == "BLP":
-#		elif cmd == "PRP":
-		elif cmd == "LSG":
-			params = command.strip().split(' ')
-			if len(params) == 4:
-				self.onAddGroup(params[1], params[2], params[3])
-			else:
-				self.onAddGroup(params[1], params[2])
-		elif cmd == "LST":
-			params = command.strip().split(' ')
-			if len(params) == 5:
-				self.onAddContact(params[1], params[2], params[3], params[3])
-			else:
-				self.onAddContact(params[1], params[2], params[3])
-#		elif cmd == "PHH":
-#		elif cmd == "PHW":
-#		elif cmd == "MOB":
-#		elif cmd == "MBE":
-#		elif cmd == "BPR":
-#		elif cmd == "ILN":
-#		elif cmd == "FLN":
-#		elif cmd == "NLN":
-#		elif cmd == "MSG":
-#		elif cmd == "RNG":
+    def execute(self, command):
+        cmd = command[:3]
+        if cmd == 'VER':
+            self.send(self.cvr())
+        elif cmd == "CVR":
+            self.send(self.usr())
+        elif cmd == "XFR":
+            host_port = command.split(' ')[3].split(':')
+            host = host_port[0]
+            port = host_port[1]
+            self.connect(host, port)
+        elif cmd == "USR":
+            params = command.split(' ')
+            if params[2] == "TWN":
+                self.authenticate(params[4])
+                self.send(self.usr())
+            elif params[2] == "OK":
+                #self.onLogged()
+                self.send(self.syn())
+        elif cmd == "SYN":
+            self.send(self.chg)
+#       elif cmd == "GTC":
+#       elif cmd == "BLP":
+#       elif cmd == "PRP":
+        elif cmd == "LSG":
+            params = command.strip().split(' ')
+            if len(params) == 4:
+                self.onAddGroup(params[1], params[2], params[3])
+            else:
+                self.onAddGroup(params[1], params[2])
+        elif cmd == "LST":
+            params = command.strip().split(' ')
+            if len(params) == 5:
+                self.onAddContact(params[1], params[2], params[3], params[3])
+            else:
+                self.onAddContact(params[1], params[2], params[3])
+#       elif cmd == "PHH":
+#       elif cmd == "PHW":
+#       elif cmd == "MOB":
+#       elif cmd == "MBE":
+#       elif cmd == "BPR":
+#       elif cmd == "ILN":
+#       elif cmd == "FLN":
+#       elif cmd == "NLN":
+#       elif cmd == "MSG":
+#       elif cmd == "RNG":
 #$this->onRing($params[1], $params[2], $params[3], $params[4], $params[5], $params[6]);
-		elif cmd == "CHG":
-			self.onConnected()
-		elif cmd == "CHL":
-			params = command.strip().split(' ');
-			self.send(self.qry(params[2]))
-		#	elif cmd == "QRY":
-		elif cmd == "207":	
-			self.disconnect()
+        elif cmd == "CHG":
+            self.onConnected()
+        elif cmd == "CHL":
+            params = command.strip().split(' ');
+            self.send(self.qry(params[2]))
+        #   elif cmd == "QRY":
+        elif cmd == "207":  
+            self.disconnect()
